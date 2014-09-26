88fe280d250aa20f9a69d6d8d1bdd904273558c7
Romain de Wolff <rdewolff@gmail.com>
2014-09-26 11:45:48 +0200

    Dynamic publish

--- server/publications.js
+++ server/publications.js
@@ -1,14 +1,13 @@
 
-
-// Autopublish must be off
-Meteor.publish('artists', function(author) {
-
-  return TempleData['artists'].find({});
-
+// Register the modules first - required!
+Meteor.publish('modules', function () {
+	return TempleSystem['modules'].find({});
 });
 
-Meteor.publish('modules', function () {
-
-  return TempleSystem['modules'].find();
-
+// dynamic publish registered modules
+TempleSystem['modules'].find({}).forEach( function(module) {
+	console.log('module', module.name);
+	Meteor.publish(module.name, function(author) {
+	  return TempleData[module.name].find({});
+	});
 });
