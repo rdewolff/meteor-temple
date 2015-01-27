079f19cc4bb2b4bf19524443dafaeedb20f2cfe5
Romain de Wolff <rdewolff@gmail.com>
2015-01-27 18:31:22 +0100

    Added todo

--- client/temple/temple.js
+++ client/temple/temple.js
@@ -38,6 +38,7 @@ UI.registerHelper("data", function(){
  */
 
 // Determine if current link should be active.
+// TODO: move to shared package
 Template.registerHelper('active', function(path) {
   // messy way : curPath=function(){var c=window.location.pathname;var b=c.slice(0,-1);var a=c.slice(-1);if(b==""){return"/"}else{if(a=="/"){return b}else{return c}}};
 
